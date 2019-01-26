1f5c3b3ac684c994cf3e32e328141d477bcdb02b
Porrith Suong <psuong95@gmail.com>
2019-01-26 11:33:17 -0500

    Adds tags to allow for querying.

---
+++ Assets/Scripts/Components/Tags/LegTagComponent.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+namespace RagdollWakeUp.Tags {
+    
+    public struct LegTag : IComponentData { }
+
+    public class LegTagComponent : ComponentDataWrapper<LegTag> { }
+}
