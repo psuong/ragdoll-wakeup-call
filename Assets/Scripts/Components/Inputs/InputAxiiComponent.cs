8db408c671d421c0a1bb1c43903c77d40114d20d
Jay Suong <jaysuong@gmail.com>
2019-01-26 11:18:44 -0500

    Adds input and id components

---
+++ Assets/Scripts/Components/Inputs/InputAxiiComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace RagdollWakeUp.Inputs {
+    /// <summary>
+    /// Represents the player's input joystick values
+    /// </summary>
+    [Serializable]
+    public struct InputAxii : ISharedComponentData {
+        public NativeArray<Vector2> Value;
+    }
+
+    public class InputAxiiComponent : SharedComponentDataWrapper<InputAxii> {
+
+        private void Awake () {
+            Value = new InputAxii { Value = new NativeArray<Vector2> (2, Allocator.Persistent) };
+        }
+
+        private void OnDestroy () {
+            var arr = Value.Value;
+
+            if (arr.IsCreated) {
+                arr.Dispose ();
+            }
+        }
+    }
+}
