d2b357145052861eb6d1867b15f3045bd255525a
Jay Suong <jaysuong@gmail.com>
2019-01-26 17:21:51 -0500

    Adds a system to orbit the camee camera over a certain point

---
+++ Assets/Scripts/Components/Cameras/CameraAxisComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace RagdollWakeUp.Cameras {
+    [Serializable]
+    public struct CameraAxis : IComponentData {
+        public Vector2 Value;
+    }
+
+    public class CameraAxisComponent : ComponentDataWrapper<CameraAxis> { }
+
+}
---
+++ Assets/Scripts/Components/Cameras/CameraOrbitComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace RagdollWakeUp.Cameras {
+    [Serializable]
+    public struct CameraOrbit : IComponentData {
+        public Vector3 Pivot;
+        public float Distance;
+        public float Height;
+        public float OrbitSensitivity;
+        public float OrbitSpeed;
+    }
+
+    public class CameraOrbitComponent : ComponentDataWrapper<CameraOrbit> { }
+}
