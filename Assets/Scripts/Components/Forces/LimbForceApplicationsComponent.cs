91601e19e0343655799a18d140978db636a8341b
dqdt <sw2384@nyu.edu>
2019-01-26 14:06:35 -0500

    adds limb force component and system

---
+++ Assets/Scripts/Components/Forces/LimbForceApplicationsComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Entities;
+
+namespace RagdollWakeUp.Forces {
+    [Serializable]
+    public struct LimbForceApplications : IComponentData {
+        public float ForceMultiplier;
+    }
+    public class LimbForceApplicationsComponent : ComponentDataWrapper<LimbForceApplications> {
+
+    }
+}
---
+++ Assets/Scripts/Systems/Forces/ApplyLimbForceSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RagdollWakeUp.Inputs;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+namespace RagdollWakeUp.Forces {
+    public class ApplyLimbForceSystem : ComponentSystem {
+        private ComponentGroup limbGroup;
+        protected override void OnCreateManager () {
+            limbGroup = GetComponentGroup (
+                ComponentType.ReadOnly<InputAxii> (),
+                ComponentType.ReadOnly<LimbForceApplications> (),
+                typeof (Rigidbody)
+            );
+        }
+        protected override void OnUpdate () {
+            var inputAxiiArray = limbGroup.GetComponentDataArray<InputAxii> ();
+            var limbForceApplicationsArray = limbGroup.GetComponentDataArray<LimbForceApplications> ();
+            var rigidBodyArray = limbGroup.GetComponentArray<Rigidbody> ();
+            int N = inputAxiiArray.Length;
+            for (int i = 0; i < N; i++) {
+                var axii = inputAxiiArray[i];
+                var forceMultiplier = limbForceApplicationsArray[i].ForceMultiplier;
+
+                var forceVec = new Vector3 (axii.LeftJoyStick.x, axii.LeftJoyStick.y, 0);
+                forceVec *= forceMultiplier;
+
+                rigidBodyArray[i].AddForce (forceVec);
+            }
+        }
+    }
+}
