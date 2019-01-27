9e3ff930483e15db9fb5ba7072f6cf5c4d6d674e
dqdt <sw2384@nyu.edu>
2019-01-27 11:19:43 -0500

    added Utilities/ManualRestartButton to reload scenes

---
+++ Assets/Scripts/Utilities/ManualRestartButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManualRestartButton : MonoBehaviour {
+    [SerializeField] public string[] scenePaths = new string[] {
+        "Assets/Scenes/SampleScene.unity"
+    };
+    void Update () {
+        if (Input.GetKeyDown ("1")) {
+
+            SceneManager.LoadScene (scenePaths[0]);
+            Debug.Log ($"loading scene \"{scenePaths[0]}\"");
+
+            for (int i = 1; i < scenePaths.Length; i++) {
+
+                SceneManager.LoadSceneAsync (scenePaths[i]);
+                Debug.Log ($"loading scene \"{scenePaths[i]}\"");
+            }
+        }
+    }
+}
