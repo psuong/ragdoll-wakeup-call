1b275f54d74e80a56da137e0d043744d38a60952
Jay Suong <jaysuong@gmail.com>
2019-02-01 15:33:21 -0500

    Adds a timer to the end game screen

--- Assets/Scripts/Components/Common/TimerComponent.cs
+++ Assets/Scripts/Components/Common/TimerComponent.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using Unity.Entities;
+using UnityEngine;
 
 namespace RagdollWakeUp {
     [Serializable]
     public struct Timer : IComponentData {
         public float Value;
+
+        public float Minutes => Mathf.Floor (Value / 60f);
     }
 
     public class TimerComponent : ComponentDataWrapper<Timer> { }
